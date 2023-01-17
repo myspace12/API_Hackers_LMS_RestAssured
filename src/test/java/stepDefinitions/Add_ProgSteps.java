b3a14044b07bf2c952eb4b16e79512a2d3031df3
myspace12 <102703361+myspace12@users.noreply.github.com>
2023-01-17 13:02:00 -0600

    Update Add_ProgSteps.java

--- src/test/java/stepDefinitions/Add_ProgSteps.java
+++ src/test/java/stepDefinitions/Add_ProgSteps.java
@@ -16,9 +16,9 @@ public class Add_ProgSteps extends Utils {
 	TestDataBuild data=new TestDataBuild();
 	
 	@Given("Add Program Payload with {string} {string} {string}")
-	public void add_program_payload_with(String programName,String programDescription,String programStatus) throws IOException {
+	public void add_program_payload_with(String programName,String programDescription,String programStatus, String creationTime, String lastModTime) throws IOException {
 	
-		req=given().spec(requestSpecification()).body(data.addProgPayload(programName,programDescription, programStatus));
+		req=given().spec(requestSpecification()).body(data.addProgPayload(programName,programDescription, programStatus, creationTime, lastModTime));
 	    
 	}}
 
