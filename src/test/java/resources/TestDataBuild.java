fae88ccd83ab68aa25395f22f2d78feffc6fef26
Agila27 <119345639+Agila27@users.noreply.github.com>
2023-01-17 10:29:41 -0500

    Update TestDataBuild.java

--- src/test/java/resources/TestDataBuild.java
+++ src/test/java/resources/TestDataBuild.java
@@ -5,6 +5,7 @@ package resources;
 import pojo_Classes.AddProg;
 
 public class TestDataBuild {
+	
 public AddProg addProgPayload(String programName,String programDescription,String programStatus)
 	
 	{
@@ -16,6 +17,33 @@ public AddProg addProgPayload(String programName,String programDescription,Strin
 		return a;
 		
 		
+	}
+	
+	public AddBatch postBatchPayload(String BatchName, String batchDes, String batchStatus, int noOfClasses, int progId,
+			String progNm) {
+		AddBatch M = new AddBatch();
+		M.setBatchName(BatchName);
+		M.setBatchDescription(batchDes);
+		M.setBatchStatus(batchStatus);
+		M.setBatchNoOfClasses(noOfClasses);
+		M.setProgramId(progId);
+		M.setProgramName(progNm);
+
+		return M;
+
+	}
+
+	public PutProg putProgPayload(String ProgramName, String ProgramDes, String ProgStatus, String creationTime, String lastModTime) {
+
+		PutProg Put = new PutProg();
+		Put.setProgramName(ProgramName);
+		Put.setProgramDescription(ProgramDes);
+		Put.setProgramStatus(ProgStatus);
+		Put.setCreationTime(creationTime);
+		Put.setLastModTime(lastModTime);
+		
+		return Put;
+
 	}
 	
   }
