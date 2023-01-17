88c707f19647e8a34b0a7b731e37b35019f7f90b
myspace12 <102703361+myspace12@users.noreply.github.com>
2023-01-17 11:16:22 -0600

    Update GetBatch.java

--- src/main/java/pojo_Classes/GetBatch.java
+++ src/main/java/pojo_Classes/GetBatch.java
@@ -1,5 +1,42 @@
 package pojo_Classes;
 
 public class GetBatch {
+	
+			
+		public class GetProg {
+			
+			private int batchId;
+			private String batchName;
+			private String batchDescription;
+			private String batchStatus;
+				
+			public void getbatchId() {
+				return;
+			}
+			public void setbatchId(int batchId) {
+				this.batchId = batchId;
+			}
+			public String getbatchName(int batchId) {
+						
+				return batchName;
+			}
+			public void setbatchName(String batchName) {
+				this.batchName = batchName;
+			}
+			public String batchDescription() {
+				return batchDescription;
+			}
+			public void setProgDesc(String batchDescription) {
+			this.batchDescription = batchDescription;
+			}
+			public String getProgStatue() {
+				return batchStatus;
+			}
+			public void setProgStatue(String batchStatus) {
+				this.batchStatus = batchStatus;
+			}
+	}
+	}
 
-}
+
+
