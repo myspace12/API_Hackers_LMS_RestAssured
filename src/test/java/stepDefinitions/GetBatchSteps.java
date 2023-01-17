527c2a63fa775a5c43ecc449c1ceadb1e32b6941
myspace12 <102703361+myspace12@users.noreply.github.com>
2023-01-17 11:20:16 -0600

    Update GetBatchSteps.java

--- src/test/java/stepDefinitions/GetBatchSteps.java
+++ src/test/java/stepDefinitions/GetBatchSteps.java
@@ -1,7 +1,24 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.Given;
 import resources.Utils;
+import static io.restassured.RestAssured.*;
+
+import java.io.IOException;
 
 public class GetBatchSteps extends Utils  {
 
+		
+
+	@Given("user has baseURI")
+	public void user_has_base_uri() throws IOException {
+		res=given().spec(requestSpecification());
+	}
+
+
+	@Given("user has valid {int}")
+	public void user_has_valid(int string) {
+	   
+	}
 }
+
