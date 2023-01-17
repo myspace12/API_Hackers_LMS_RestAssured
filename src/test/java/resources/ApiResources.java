20c2d1cc84a2bec2e7e6d83f6d5800389afee889
myspace12 <102703361+myspace12@users.noreply.github.com>
2023-01-17 12:54:38 -0600

    Update ApiResources.java

--- src/test/java/resources/ApiResources.java
+++ src/test/java/resources/ApiResources.java
@@ -19,34 +19,7 @@ public enum ApiResources {
 	DeleteBatchByldAP("/deletebyprogid/");
 
 	
-	
-	GetProg_byIdAPI("/programs/"),
-
-	AddProgAPI("/saveprogram"),
-	
-	PutProgbyIdAPI("/putprogram/"),
-	
-	PutProgbyNmAPI("/program/"),
-	
-    DeleteProgramByIDAPI("/deletebyprogname/"),
-	
-	DeleteProgramByNameAPI("/deletebyprogname/"),
-	
-	AddBatchAPI("/batches"),
-	
-	GetBatchAPI("/batches"),
-	
-	GetBatchByNmAPI("/batches/batchName"),
-	
-	GetBatchByPrIdAPI("/batches/program"),
-	
-	PutBatchAPI("/batches/"),
-	
-	DeleteBatchByIdAPI("/deletebyprogid");
-
-		
-
-			
+				
     private String resource;
 	
 	 ApiResources(String resource)
